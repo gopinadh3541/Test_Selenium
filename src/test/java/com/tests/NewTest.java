2d430adb3bf577d43f63e5e35b362e77f2bae4a9
gopinadh3541 <39827201+gopinadh3541@users.noreply.github.com>
2019-02-14 15:44:33 +0530

    Update NewTest.java

--- src/test/java/com/tests/NewTest.java
+++ src/test/java/com/tests/NewTest.java
@@ -33,18 +33,20 @@ public class NewTest {
 	  
 	  driver.get("http://10.89.44.105:9001/BankExample/");
 	 
-	  driver.manage().window().maximize();
-	  String title = driver.getTitle();
-	  driver.getCurrentUrl();
-	  String expectedTitle="Banking";
-	  Assert.assertEquals(expectedTitle, title);
-		System.out.println("After Assertion:"+title+" "+expectedTitle+" Matched");
+	  
+		driver.manage().window().maximize();
 		WebElement Searchbox = driver.findElement(By.name("uname"));
+		WebElement Searchbox1=driver.findElement(By.name("pwd"));
 		Searchbox.sendKeys("admin");
-		WebElement Searchbox1= driver.findElement(By.name("pwd"));
 		Searchbox1.sendKeys("admin");
-	  	Searchbox.submit();
-		System.out.println("after click");
+		WebElement Searchbox2=driver.findElement(By.name("submit"));
+		Searchbox2.submit();
+		System.out.println("After Click");
+		String title = driver.getTitle();
+		driver.getCurrentUrl();
+		String expectedTitle = "Frames";
+		Assert.assertEquals(expectedTitle, title);
+		System.out.println("After Assertion:" + title + " " + expectedTitle + " Matched");
 	;
 	 
 	  
